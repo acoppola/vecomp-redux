d0825b0fd428c1eb87dddf506fb9463f200e59ba
Jiayi Hu <jiayi.ghu@gmail.com>
2017-02-26 22:05:03 +0100

    Add initial stub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AddOperationComponent } from './components/add-operation/add-operation.component';
+import { OperationsComponent } from './components/operations/operations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddOperationComponent,
+    OperationsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/add-operation/add-operation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'add-operation',
+  templateUrl: './add-operation.component.html'
+})
+export class AddOperationComponent {
+
+}
